744f73f9e8bbeb528bec36db4444facb6f4d6819
Pawel Rozniecki <pawelrozniecki13@outlook.com>
2020-01-13 21:36:07 +0100

    added new layers

--- src/model.py
+++ src/model.py
@@ -4,7 +4,25 @@ from torch import nn
 class Model(nn.Module):
     def __init__(self, original_model, num_classes):
         super(Model, self).__init__()
-        self.features = original_model.features
+        # self.features = original_model.features
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=False),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
 
         self.classifier = nn.Sequential(
             nn.Dropout(),
