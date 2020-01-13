642f37b4a182109b39d9bad12fd7397c55195c90
Pawel Rozniecki <pawelrozniecki13@outlook.com>
2020-01-13 23:06:39 +0100

    daga

--- src/model.py
+++ src/model.py
@@ -30,6 +30,12 @@ class Model(nn.Module):
             nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes)
         )
 
