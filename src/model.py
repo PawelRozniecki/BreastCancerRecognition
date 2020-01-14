2c4b45a3d7e4c57f9a7cad139ee690581634c75c
Pawel Rozniecki <pawelrozniecki13@outlook.com>
2020-01-14 11:54:37 +0100

    added batchNorm2d

--- src/model.py
+++ src/model.py
@@ -9,14 +9,16 @@ class Model(nn.Module):
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.BatchNorm2d(192),
             nn.ReLU(inplace=False),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.BatchNorm2d(384),
             nn.ReLU(inplace=True),
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
-
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
