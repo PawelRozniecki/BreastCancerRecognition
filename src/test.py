fb7688897bdce0ca8febe872201f6011408700bd
Pawel <pawelrozniecki13@outlook.com>
2020-01-27 12:13:34 +0100

    Update test.py

--- src/test.py
+++ src/test.py
@@ -22,7 +22,6 @@ def main():
 
     transform = transforms.Compose([
         transforms.Resize((128,128)),
-        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
 
